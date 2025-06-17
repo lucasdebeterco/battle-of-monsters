a7ece346d53440cb14402bf57f61cbb6639533a3
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2025-06-17 01:13:12 -0300

    Revert "Tests implement"
    This reverts commit 32cfa04892c4cfb1846030f02868769ef38dddb9.

--- src/components/winner-display/WinnerDisplay.extended.spec.tsx
+++ src/components/winner-display/WinnerDisplay.extended.spec.tsx
@@ -1,13 +1,9 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import WinnerDisplay from './WinnerDisplay';
-import { Monster } from '../../models/interfaces/monster.interface';
 
 // TODO complete tests
 describe('WinnerDisplayExtended', () => {
   it('renders the winner text correctly', () => {
-    const winner: Monster = { id: '1', name: 'Monster A', defense: 10, attack: 10, speed: 10, hp: 10, imageUrl: '' };
-    render(<WinnerDisplay winner={winner} />);
-    expect(screen.getByText(`${winner.name} wins!`)).toBeInTheDocument();
+    // TODO - complete the test
+    expect(1).toEqual(2);
   });
 });
--- src/reducers/monsters/monsters.reducer.extended.spec.ts
+++ src/reducers/monsters/monsters.reducer.extended.spec.ts
@@ -1,35 +1,18 @@
 import monstersData from '../../../data/monsters.json';
-import { fetchBattle, fetchRandomMonster } from './monsters.actions';
-import monsterReducer, { MonstersState, initialState } from './monsters.reducer';
 
 describe('Monsters Reducer', () => {
   it('should change the battle on action fulfilled', () => {
-    const mockBattle = { winner: { name: 'Monster A' }, battle: { monster1: { name: 'Monster A' }, monster2: { name: 'Monster B' } } };
-    const action = {
-      type: fetchBattle.fulfilled.type,
-      payload: mockBattle,
-    };
-    const newState = monsterReducer(initialState, action);
-    expect(newState.battle).toEqual(mockBattle);
+    // TODO
+    expect(1).toEqual(2);
   });
 
   it('should add the random monster to the state', () => {
-    const mockMonster = monstersData[0];
-    const action = {
-      type: fetchRandomMonster.fulfilled.type,
-      payload: mockMonster,
-    };
-    const newState: MonstersState = monsterReducer(initialState, action);
-    expect(newState.selectedMonster).toEqual(mockMonster);
+    // TODO
+    expect(1).toEqual(2);
   });
 
   it('should add the winner to the state', () => {
-    const mockWinner = { name: 'Monster A' };
-    const action = {
-      type: fetchBattle.fulfilled.type,
-      payload: { winner: mockWinner, battle: {} },
-    };
-    const newState: MonstersState = monsterReducer(initialState, action);
-    expect(newState.winner).toEqual(mockWinner);
+    // TODO
+    expect(1).toEqual(2);
   });
 });
